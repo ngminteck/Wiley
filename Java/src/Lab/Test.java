bf5ed7fbcfc164610e478c9d77eccf0807b72a77
ClarenceNgMinTeck <ng_min_teck@hotmail.com>
2022-06-14 17:32:48 +0800

    update
    update

--- Java/src/Lab/Test.java
+++ Java/src/Lab/Test.java
@@ -30,6 +30,34 @@ interface Interface3 extends Interface1 , Interface2
     void Test3();
 }
 
+class TestClassA
+{
+    public static void method4()
+    {
+        System.out.println("A");
+    }
+
+    public  void method2()
+    {
+
+    }
+
+
+}
+
+class TestClassB extends TestClassA
+{
+    public static void method4()
+    {
+        System.out.println("B");
+    }
+
+    public void method2()
+    {
+
+    }
+}
+
 // no point implement interface 1 cos interface 3 already included
 public class Test extends Abstract implements Interface3, Interface1 {
 
@@ -49,11 +77,20 @@ public class Test extends Abstract implements Interface3, Interface1 {
         System.out.println("Class Test3");
     }
 
+
+
     public static void main(String[] args)
     {
         Test test = new Test();
         test.Test1();
         test.Test2();
         test.Test3();
+
+        TestClassB tcb = new TestClassB();
+        tcb.method2();
+        tcb.method4();
+
+        TestClassA tca = new TestClassB();
+        tca.method4();
     }
 }
