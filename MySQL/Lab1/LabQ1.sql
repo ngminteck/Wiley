e93ffb668bd829d5505cac633200e5285bcedd55
ClarenceNgMinTeck <ng_min_teck@hotmail.com>
2022-06-22 15:59:44 +0800

    Update LabQ1.sql
    update

--- MySQL/Lab1/LabQ1.sql
+++ MySQL/Lab1/LabQ1.sql
@@ -4,6 +4,14 @@
 -- WHERE rating IN("R","NC-17"); OR
 -- WHERE rating NOT IN("R","NC-17"); AND
 
+-- select department_id,count(*) as Counting
+-- from employees 
+-- where department_id in (50,80,100,20)
+-- group by department_id
+-- having Counting >5
+-- order by Counting;
+
+
 USE hr;
 
 SELECT * FROM employees
@@ -19,5 +27,27 @@ SELECT * FROM employees
 WHERE department_id ="60"
 ORDER BY first_name DESC;
 
+select * 
+from employees e
+join departments d on e.department_id = d.department_id
+join locations l on d.location_id = l.location_id
+group by e.employee_id;
+
+select * 
+from employees
+where hire_date  >= "2004-01-01";
+
+select *
+from employees
+where salary = (select max(salary)  from employees );
+
+
+select *
+from employees
+where hire_date  >=  (select hire_date from employees where first_name ="Bruce" );
+
+select * 
+from employees
+where  (hire_date between '2004-06-01' and '2004-06-01')
 
 
