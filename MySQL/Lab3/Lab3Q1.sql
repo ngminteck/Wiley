40eb0ff006ae7a8ec3b982122d606456db66cb0c
ClarenceNgMinTeck <ng_min_teck@hotmail.com>
2022-06-24 11:58:15 +0800

    Update Lab3Q1.sql

--- MySQL/Lab3/Lab3Q1.sql
+++ MySQL/Lab3/Lab3Q1.sql
@@ -37,6 +37,12 @@ from employees e
 left outer join employees m
 on e.manager_id = m.employee_id;
 
+select concat_ws(" ",e.first_name, e.last_name) as employees_name, ifnull(concat_ws(" ",m.first_name, m.last_name),'None') as manager_name
+from employees e
+left outer join employees m
+on e.manager_id = m.employee_id;
+
+
 -- inner join 1
 -- left outer join 3
 -- right outer join 3
