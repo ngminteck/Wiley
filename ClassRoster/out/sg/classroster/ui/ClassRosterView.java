84c3fff5ff891e499d549ddb4ccc70589edc2b1c
ClarenceNgMinTeck <ng_min_teck@hotmail.com>
2022-06-28 13:58:14 +0800

    update
    update

---
+++ ClassRoster/out/sg/classroster/ui/ClassRosterView.java
@@ -0,0 +1,141 @@
+package com.sg.classroster.ui;
+
+import com.sg.classroster.dto.Student;
+
+import java.util.List;
+
+public class ClassRosterView {
+    private UserIO io;
+
+    public ClassRosterView(UserIO io) {
+        this.io = io;
+    }
+
+    public int printMenuAndGetSelection() {
+        io.print("Main Menu");
+        io.print("1. List Students");
+        io.print("2. Create New Student");
+        io.print("3. View a Student");
+        io.print("4. Remove a Student");
+        io.print("5. Exit");
+
+        return io.readInt("Please select from the above choices.", 1, 5);
+    }
+
+    public void run() {
+        boolean keepGoing = true;
+        int menuSelection = 0;
+        while (keepGoing) {
+            io.print("Main Menu");
+            io.print("1. List Student IDs");
+            io.print("2. Create New Student");
+            io.print("3. View a Student");
+            io.print("4. Remove a Student");
+            io.print("5. Exit");
+
+            menuSelection = io.readInt("Please select from the"
+                    + " above choices.", 1, 5);
+
+            switch (menuSelection) {
+                case 1:
+                    io.print("LIST STUDENTS");
+                    break;
+                case 2:
+                    io.print("CREATE STUDENT");
+                    break;
+                case 3:
+                    io.print("VIEW STUDENT");
+                    break;
+                case 4:
+                    io.print("REMOVE STUDENT");
+                    break;
+                case 5:
+                    keepGoing = false;
+                    break;
+                default:
+                    io.print("UNKNOWN COMMAND");
+            }
+
+        }
+        io.print("GOOD BYE");
+    }
+    public Student getNewStudentInfo() {
+        String studentId = io.readString("Please enter Student ID");
+        String firstName = io.readString("Please enter First Name");
+        String lastName = io.readString("Please enter Last Name");
+        String cohort = io.readString("Please enter Cohort");
+        Student currentStudent = new Student(studentId);
+        currentStudent.setFirstName(firstName);
+        currentStudent.setLastName(lastName);
+        currentStudent.setCohort(cohort);
+        return currentStudent;
+    }
+
+    public void displayCreateStudentBanner() {
+        io.print("=== Create Student ===");
+    }
+
+    public void displayCreateSuccessBanner() {
+        io.readString(
+                "Student successfully created.  Please hit enter to continue");
+    }
+
+    public void displayStudentList(List<Student> studentList) {
+        for (Student currentStudent : studentList) {
+            String studentInfo = String.format("#%s : %s %s",
+                    currentStudent.getStudentId(),
+                    currentStudent.getFirstName(),
+                    currentStudent.getLastName());
+            io.print(studentInfo);
+        }
+        io.readString("Please hit enter to continue.");
+    }
+
+    public void displayDisplayAllBanner() {
+        io.print("=== Display All Students ===");
+    }
+
+    public void displayDisplayStudentBanner () {
+        io.print("=== Display Student ===");
+    }
+
+    public String getStudentIdChoice() {
+        return io.readString("Please enter the Student ID.");
+    }
+    public void displayRemoveStudentBanner () {
+        io.print("=== Remove Student ===");
+    }
+
+    public void displayRemoveResult(Student studentRecord) {
+        if(studentRecord != null){
+            io.print("Student successfully removed.");
+        }else{
+            io.print("No such student.");
+        }
+        io.readString("Please hit enter to continue.");
+    }
+
+    public void displayStudent(Student student) {
+        if (student != null) {
+            io.print(student.getStudentId());
+            io.print(student.getFirstName() + " " + student.getLastName());
+            io.print(student.getCohort());
+            io.print("");
+        } else {
+            io.print("No such student.");
+        }
+        io.readString("Please hit enter to continue.");
+    }
+
+    public void displayExitBanner() {
+        io.print("Good Bye!!!");
+    }
+
+    public void displayUnknownCommandBanner() {
+        io.print("Unknown Command!!!");
+    }
+    public void displayErrorMessage(String errorMsg) {
+        io.print("=== ERROR ===");
+        io.print(errorMsg);
+    }
+}
